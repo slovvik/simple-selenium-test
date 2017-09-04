9d61255f05625cb5019cfc22af1899bd1d4642b6
slovvik <slowikbartek@gmail.com>
2017-09-04 21:11:44 +0200

    Added simple google search test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,2 +1,41 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class SimpleTest {
+
+    public static final String GOOGLE_URL = "http://www.google.com";
+    public static final String SEARCHING_PHRASE = "http://slovvik.blogspot.com/";
+
+    static {
+        System.setProperty("webdriver.chrome.driver", "D:\\Projects\\chromedriver.exe");
+    }
+
+    private WebDriver driver = new ChromeDriver();
+    private WebDriverWait webDriverWait;
+
+    @Test
+    public void simpleGoogleTest() {
+        driver.get(GOOGLE_URL);
+        driver.manage().window().maximize();
+        webDriverWait = new WebDriverWait(driver, 5);
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys(SEARCHING_PHRASE);
+        WebElement searchButton = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.name("btnK")));
+        searchButton.click();
+        String pageTitle = driver.getTitle();
+        Assert.assertThat(pageTitle, CoreMatchers.containsString(SEARCHING_PHRASE));
+    }
+
+    @After
+    public void closeDriver() {
+        driver.quit();
+    }
 }
